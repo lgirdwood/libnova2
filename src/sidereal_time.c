14ad2d98ebbd1b4a29df99da7be7b9a141fdeeda
Liam Girdwood <lgirdwood@gmail.com>
2026-02-09 22:28:51 +0000

    refactor: Change sidereal time calculations to output in hours instead of radians.

--- src/sidereal_time.c
+++ src/sidereal_time.c
@@ -12,69 +12,65 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
- *  
+ *
  *  Copyright (C) 2000 - 2005 Liam Girdwood <lgirdwood@gmail.com>
  */
 
-#include <math.h>
-#include <libnova/sidereal_time.h>
 #include <libnova/nutation.h>
+#include <libnova/sidereal_time.h>
 #include <libnova/utility.h>
+#include <math.h>
 
 /*! \fn double ln_get_mean_sidereal_time(double JD)
-* \param JD Julian Day
-* \return Mean sidereal time.
-*
-* Calculate the mean sidereal time at the meridian of Greenwich of a given date.
-*/
-/* Formula 11.1, 11.4 pg 83 
-*/
+ * \param JD Julian Day
+ * \return Mean sidereal time.
+ *
+ * Calculate the mean sidereal time in hours at the meridian of Greenwich of a
+ * given date.
+ */
+/* Formula 11.1, 11.4 pg 83
+ */
+double ln_get_mean_sidereal_time(double JD) {
+  long double sidereal;
+  long double T;
 
-double ln_get_mean_sidereal_time(double JD)
-{
-	long double sidereal;
-	long double T;
+  T = (JD - 2451545.0) / 36525.0;
 
-	T =(JD - 2451545.0) / 36525.0;
+  /* calc mean angle in degrees */
+  sidereal = 280.46061837 + (360.98564736629 * (JD - 2451545.0)) +
+             (0.000387933 * T * T) - (T * T * T / 38710000.0);
+  /* add a convenient multiple of 360 degrees */
+  sidereal = ln_range_degrees(sidereal);
+  /* change to hours */
+  sidereal = sidereal / 360.0 * 24.0;
 
-	/* calc mean angle */
-	sidereal = 280.46061837 + (360.98564736629 *(JD - 2451545.0)) +
-		(0.000387933 * T * T) - (T * T * T / 38710000.0);
-
-	/* add a convenient multiple of 360 degrees */
-	sidereal = ln_range_degrees(sidereal);
-
-	/* change to radians */
-	sidereal = ln_deg_to_rad(sidereal);
-
-	return sidereal;
-} 
+  return sidereal;
+}
 
 /*! \fn double ln_get_apparent_sidereal_time(double JD)
-* \param JD Julian Day
-* /return Apparent sidereal time (hours).
-*
-* Calculate the apparent sidereal time at the meridian of Greenwich of a given date. 
-*/
-/* Formula 11.1, 11.4 pg 83 
-*/
+ * \param JD Julian Day
+ * /return Apparent sidereal time (hours).
+ *
+ * Calculate the apparent sidereal time at the meridian of Greenwich of a given
+ * date.
+ */
+/* Formula 11.1, 11.4 pg 83
+ */
 
-double ln_get_apparent_sidereal_time(double JD)
-{
-	double correction, sidereal;
-	struct ln_nutation nutation;
+double ln_get_apparent_sidereal_time(double JD) {
+  double correction, sidereal;
+  struct ln_nutation nutation;
 
-	/* get the mean sidereal time */
-	sidereal = ln_get_mean_sidereal_time(JD);
+  /* get the mean sidereal time */
+  sidereal = ln_get_mean_sidereal_time(JD);
 
-	/* add corrections for nutation in longitude and for the true obliquity of
-	the ecliptic */
-	ln_get_nutation(JD, &nutation);
+  /* add corrections for nutation in longitude and for the true obliquity of
+  the ecliptic */
+  ln_get_nutation(JD, &nutation);
 
-	correction = (nutation.longitude *
-	cos(nutation.obliquity));
+  correction = (nutation.longitude * cos(nutation.obliquity));
 
-	sidereal += correction;
+  sidereal += correction;
 
-	return sidereal;
-	}
+  return sidereal;
+}

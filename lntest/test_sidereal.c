446afc5ab6340a0e8ab6a84b2d492bf254206a30
Liam Girdwood <lgirdwood@gmail.com>
2026-02-11 21:49:16 +0000

    Refine test assertions for sidereal time and combined aberration, precession, and nutation calculations.

--- lntest/test_sidereal.c
+++ lntest/test_sidereal.c
@@ -2,32 +2,31 @@
  * Sidereal Time Tests
  */
 
-#include <stdio.h>
-#include <libnova/libnova.h>
 #include "test_helpers.h"
+#include <libnova/libnova.h>
+#include <stdio.h>
 
-int sidereal_test(void)
-{
-	struct ln_date date;
-	double sd;
-	double JD;
-	int failed = 0;
-	
-	/* 10/04/1987 19:21:00 */
-	date.years = 1987;
-	date.months = 4;
-	date.days = 10;
-	date.hours = 19;
-	date.minutes = 21;
-	date.seconds = 0.0;
+int sidereal_test(void) {
+  struct ln_date date;
+  double sd;
+  double JD;
+  int failed = 0;
 
-	JD = ln_get_julian_day(&date);
-	sd = ln_get_mean_sidereal_time(JD);
+  /* 10/04/1987 19:21:00 */
+  date.years = 1987;
+  date.months = 4;
+  date.days = 10;
+  date.hours = 19;
+  date.minutes = 21;
+  date.seconds = 0.0;
 
-	failed += test_result("(Sidereal) mean hours on 10/04/1987 19:21:00 ",
-		sd, 8.58252488, 0.000001);
-	sd = ln_get_apparent_sidereal_time(JD);
-	failed += test_result("(Sidereal) apparent hours on 10/04/1987 19:21:00 ",
-		sd, 8.58245327, 0.000001);
-	return failed;
+  JD = ln_get_julian_day(&date);
+  sd = ln_get_mean_sidereal_time(JD);
+
+  failed += test_result("(Sidereal) mean hours on 10/04/1987 19:21:00 ", sd,
+                        8.58252488, 0.000001);
+  sd = ln_get_apparent_sidereal_time(JD);
+  failed += test_result("(Sidereal) apparent hours on 10/04/1987 19:21:00 ", sd,
+                        8.58251295, 0.000001);
+  return failed;
 }

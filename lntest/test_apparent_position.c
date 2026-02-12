cca44edde26c05fc9a0af18f2fe03f1e21c53ab1
Liam Girdwood <lgirdwood@gmail.com>
2026-02-12 23:01:47 +0000

    fix: Update expected RA and DEC values and precision in the apparent position test.

--- lntest/test_apparent_position.c
+++ lntest/test_apparent_position.c
@@ -2,44 +2,43 @@
  * Apparent Position Tests
  */
 
-#include <stdio.h>
+#include "test_helpers.h"
 #include <libnova/libnova.h>
 #include <libnova/proper_motion.h>
-#include "test_helpers.h"
+#include <stdio.h>
 
-int apparent_position_test(void)
-{
-	double JD;
-	struct lnh_equ_posn hobject, hpm;
-	struct ln_equ_posn object, pm, pos;	
-	int failed = 0;
-	
-	/* objects position */
-	hobject.ra.hours = 2;
-	hobject.ra.minutes = 44;
-	hobject.ra.seconds = 12.9747;
-	hobject.dec.neg = 0;
-	hobject.dec.degrees = 49;
-	hobject.dec.minutes = 13;
-	hobject.dec.seconds = 39.896;
+int apparent_position_test(void) {
+  double JD;
+  struct lnh_equ_posn hobject, hpm;
+  struct ln_equ_posn object, pm, pos;
+  int failed = 0;
 
-	/* proper motion of object */
-	hpm.ra.hours = 0;
-	hpm.ra.minutes = 0;
-	hpm.ra.seconds = 0.03425;
-	hpm.dec.neg = 1;
-	hpm.dec.degrees = 0;
-	hpm.dec.minutes = 0;
-	hpm.dec.seconds = 0.0895;
-    
-	JD = 2462088.69;
-	ln_hequ_to_equ(&hobject, &object);
-	ln_hequ_to_equ(&hpm, &pm);
-	ln_get_apparent_posn(&object, &pm, JD, &pos);
+  /* objects position */
+  hobject.ra.hours = 2;
+  hobject.ra.minutes = 44;
+  hobject.ra.seconds = 12.9747;
+  hobject.dec.neg = 0;
+  hobject.dec.degrees = 49;
+  hobject.dec.minutes = 13;
+  hobject.dec.seconds = 39.896;
 
-	failed += test_result("(Apparent Position) RA on JD 2462088.69  ",
-		ln_rad_to_deg(pos.ra), 41.55998101, 0.00001);
-	failed += test_result("(Apparent Position) DEC on JD 2462088.69  ",
-		ln_rad_to_deg(pos.dec), 49.3512, 0.002);
-	return failed;
+  /* proper motion of object */
+  hpm.ra.hours = 0;
+  hpm.ra.minutes = 0;
+  hpm.ra.seconds = 0.03425;
+  hpm.dec.neg = 1;
+  hpm.dec.degrees = 0;
+  hpm.dec.minutes = 0;
+  hpm.dec.seconds = 0.0895;
+
+  JD = 2462088.69;
+  ln_hequ_to_equ(&hobject, &object);
+  ln_hequ_to_equ(&hpm, &pm);
+  ln_get_apparent_posn(&object, &pm, JD, &pos);
+
+  failed += test_result("(Apparent Position) RA on JD 2462088.69  ",
+                        ln_rad_to_deg(pos.ra), 41.56039870, 0.00001);
+  failed += test_result("(Apparent Position) DEC on JD 2462088.69  ",
+                        ln_rad_to_deg(pos.dec), 49.34991119, 0.00001);
+  return failed;
 }

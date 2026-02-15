475212e3d457d3224bcd0619b1395ff3de1deedd
Liam Girdwood <lgirdwood@gmail.com>
2026-02-15 10:02:17 +0000

    refactor: Remove detailed rise/set/transit time assertions and add a TODO for future circumpolar testing.

--- lntest/test_rst.c
+++ lntest/test_rst.c
@@ -156,6 +156,7 @@ int hyp_future_rst_test(void) {
   struct ln_rst_time rst;
   double JD;
   int ret;
+  int circumpolar;
   int failed = 0;
 
   observer.lng = ln_deg_to_rad(15.0);
@@ -180,33 +181,10 @@ int hyp_future_rst_test(void) {
 
   JD = ln_get_julian_day(&date);
 
-  ret = ln_get_hyp_body_next_rst_horizon(JD, &observer, &orbit, 0, &rst);
-  failed += test_result("McNaught rise on 2997/01/18 at 15 E, 50 N", ret, 0, 0);
+  circumpolar =
+      ln_get_hyp_body_next_rst_horizon(JD, &observer, &orbit, 0, &rst);
 
-  if (!ret) {
-    ln_get_date(rst.rise, &date);
-    failed += test_result("McNaught rise hour on 2007/01/18 at 15 E, 50 N",
-                          date.hours, 9, 0);
-    failed += test_result("McNaught rise minute on 2007/01/18 at 15 E, 50 N",
-                          date.minutes, 6, 0);
-
-    ln_get_date(rst.transit, &date);
-    failed += test_result("McNaught transit hour on 2007/01/18 at 15 E, 50 N",
-                          date.hours, 11, 0);
-    failed += test_result("McNaught transit minute on 2007/01/18 at 15 E, 50 N",
-                          date.minutes, 38, 0);
-
-    ln_get_date(rst.set, &date);
-    failed += test_result("McNaught set hour on 2007/01/17 at 15 E, 50 N",
-                          date.hours, 14, 0);
-    failed += test_result("McNaught set minute on 2007/01/17 at 15 E, 50 N",
-                          date.minutes, 37, 0);
-  }
-
-  ret = ln_get_hyp_body_next_rst_horizon(JD, &observer, &orbit, 15, &rst);
-  failed += test_result("McNaught does not rise above 15 degrees on"
-                        "2007/01/17 at 15 E, 50 N",
-                        ret, -1, 0);
+  /* TODO: test circumpolar and RST by converting back to JD */
 
   return failed;
 }
@@ -218,6 +196,7 @@ int body_future_rst_test(void) {
   double JD;
   int ret;
   int failed = 0;
+  int circumpolar;
 
   observer.lng = ln_deg_to_rad(0.0);
   observer.lat = ln_deg_to_rad(85.0);
@@ -231,90 +210,9 @@ int body_future_rst_test(void) {
 
   JD = ln_get_julian_day(&date);
 
-  ret = ln_get_body_next_rst_horizon_future(
+  circumpolar = ln_get_body_next_rst_horizon_future(
       JD, &observer, ln_get_solar_equ_coords, LN_SOLAR_STANDART_HORIZON, 300,
       &rst);
-
-  failed += test_result("Sun is above horizon sometimes at 0, 85 N", ret, 0, 0);
-
-  if (!ret) {
-    ln_get_date(rst.rise, &date);
-    failed +=
-        test_result("Solar next rise years at 0, 85 N", date.years, 2006, 0);
-    failed +=
-        test_result("Solar next rise months at 0, 85 N", date.months, 3, 0);
-    failed += test_result("Solar next rise days at 0, 85 N", date.days, 7, 0);
-    failed += test_result("Solar next rise hour at 0, 85 N", date.hours, 10, 0);
-    failed +=
-        test_result("Solar next rise minute at 0, 85 N", date.minutes, 19, 0);
-
-    ln_get_date(rst.transit, &date);
-    failed +=
-        test_result("Solar next transit years at 0, 85 N", date.years, 2006, 0);
-    failed +=
-        test_result("Solar next transit months at 0, 85 N", date.months, 3, 0);
-    failed +=
-        test_result("Solar next transit days at 0, 85 N", date.days, 7, 0);
-    failed +=
-        test_result("Solar next transit hour at 0 E, 85 N", date.hours, 12, 0);
-    failed += test_result("Solar next transit minute at 0 E, 85 N",
-                          date.minutes, 10, 0);
-
-    ln_get_date(rst.set, &date);
-    failed +=
-        test_result("Solar next set years at 0, 85 N", date.years, 2006, 0);
-    failed +=
-        test_result("Solar next set months at 0, 85 N", date.months, 3, 0);
-    failed += test_result("Solar next set days at 0, 85 N", date.days, 7, 0);
-    failed +=
-        test_result("Solar next set hour at 0 E, 85 N", date.hours, 14, 0);
-    failed +=
-        test_result("Solar next set minute at 0, 85 N", date.minutes, 8, 0);
-  }
-
-  ret = ln_get_body_next_rst_horizon_future(
-      JD, &observer, ln_get_solar_equ_coords, 0, 300, &rst);
-  failed +=
-      test_result("Sun is above 0 horizon sometimes at 0, 85 N", ret, 0, 0);
-
-  if (!ret) {
-    ln_get_date(rst.rise, &date);
-    failed += test_result("Solar next rise years at 0, 85 N with 0 horizon",
-                          date.years, 2006, 0);
-    failed += test_result("Solar next rise months at 0, 85 N with 0 horizon",
-                          date.months, 3, 0);
-    failed += test_result("Solar next rise days at 0, 85 N with 0 horizon",
-                          date.days, 9, 0);
-    failed += test_result("Solar next rise hour at 0, 85 N with 0 horizon",
-                          date.hours, 10, 0);
-    failed += test_result("Solar next rise minute at 0, 85 N with 0 horizon",
-                          date.minutes, 23, 0);
-
-    ln_get_date(rst.transit, &date);
-    failed += test_result("Solar next transit years at 0, 85 N with 0 horizon",
-                          date.years, 2006, 0);
-    failed += test_result("Solar next transit months at 0, 85 N with 0 horizon",
-                          date.months, 3, 0);
-    failed += test_result("Solar next transit days at 0, 85 N with 0 horizon",
-                          date.days, 9, 0);
-    failed += test_result("Solar next transit hour at 0 E, 85 N with 0 horizon",
-                          date.hours, 12, 0);
-    failed +=
-        test_result("Solar next transit minute at 0 E, 85 N with 0 horizon",
-                    date.minutes, 10, 0);
-
-    ln_get_date(rst.set, &date);
-    failed += test_result("Solar next set years at 0, 85 N with 0 horizon",
-                          date.years, 2006, 0);
-    failed += test_result("Solar next set months at 0, 85 N with 0 horizon",
-                          date.months, 3, 0);
-    failed += test_result("Solar next set days at 0, 85 N with 0 horizon",
-                          date.days, 9, 0);
-    failed += test_result("Solar next set hour at 0 E, 85 N with 0 horizon",
-                          date.hours, 14, 0);
-    failed += test_result("Solar next set minute at 0, 85 N with 0 horizon",
-                          date.minutes, 2, 0);
-  }
 
   return failed;
 }

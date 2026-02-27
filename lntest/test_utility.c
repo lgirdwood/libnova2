0bed89ac4c8cedb3f9d0b50b757c294cca6b255f
Liam Girdwood <lgirdwood@gmail.com>
2026-02-27 21:37:26 +0000

    refactor: improve utility test reporting and remove verbose output from VSOP87 tests.

--- lntest/test_utility.c
+++ lntest/test_utility.c
@@ -3,26 +3,36 @@
  */
 
 #include <stdio.h>
-#include <string.h>
 #include <math.h>
 #include <libnova2/libnova2.h>
 #include "test_helpers.h"
 
 int utility_test(void)
 {
+	int failed = 0;
 	struct ln_dms dms;
 	double deg = -1.23, deg2 = 1.23, deg3 = -0.5;
-	
+	char buf[256];
+
 	ln2_deg_to_dms(deg, &dms);
-	printf("TEST deg %f ==> deg %c%d min %d sec %f\n",
-		deg, dms.neg ? '-' : '+', dms.degrees, dms.minutes, dms.seconds);
+	snprintf(buf, sizeof(buf), "deg %c%d min %d sec %f", dms.neg ? '-' : '+',
+			 dms.degrees, dms.minutes, dms.seconds);
+	failed += test_str_result("(Utility) deg_to_dms -1.23", buf,
+							  "deg -1 min 13 sec 48.000000");
+
 	ln2_deg_to_dms(deg2, &dms);
-	printf("TEST deg %f ==> deg %c%d min %d sec %f\n",
-		deg2, dms.neg ? '-' : '+', dms.degrees, dms.minutes, dms.seconds);
+	snprintf(buf, sizeof(buf), "deg %c%d min %d sec %f", dms.neg ? '-' : '+',
+			 dms.degrees, dms.minutes, dms.seconds);
+	failed += test_str_result("(Utility) deg_to_dms 1.23", buf,
+							  "deg +1 min 13 sec 48.000000");
+
 	ln2_deg_to_dms(deg3, &dms);
-	printf("TEST deg %f ==> deg %c%d min %d sec %f\n",
-		deg3, dms.neg ? '-' : '+', dms.degrees, dms.minutes, dms.seconds);
-	return 0;
+	snprintf(buf, sizeof(buf), "deg %c%d min %d sec %f", dms.neg ? '-' : '+',
+			 dms.degrees, dms.minutes, dms.seconds);
+	failed += test_str_result("(Utility) deg_to_dms -0.5", buf,
+							  "deg -0 min 30 sec 0.000000");
+
+	return failed;
 }
 
 int utility_conversion_test(void)
@@ -35,7 +45,7 @@ int utility_conversion_test(void)
 	struct ln_lnlat_posn lnlat;
 	/* const char *version; */
 	const char *loc_str;
-	
+
 	/* Degrees <-> Radians */
 	val = 180.0;
 	res = LN_D2R(val);
@@ -49,7 +59,7 @@ int utility_conversion_test(void)
 	val = 370.0;
 	res = ln2_range_degrees(val);
 	failed += test_result("(Utility) range_degrees 370", res, 10.0, 1e-6);
-	
+
 	val = -10.0;
 	res = ln2_range_degrees(val);
 	failed += test_result("(Utility) range_degrees -10", res, 350.0, 1e-6);
@@ -61,17 +71,25 @@ int utility_conversion_test(void)
 	val = -M_PI;
 	res = ln2_range_radians(val);
 	failed += test_result("(Utility) range_radians -PI", res, M_PI, 1e-6);
-	
+
 	val = 3.0 * M_PI;
 	res = ln2_range_radians2(val);
 	failed += test_result("(Utility) range_radians2 3PI", res, M_PI, 1e-6);
 
 	/* Human readable horizontal */
-	hlnlat.lng.degrees = 100; hlnlat.lng.minutes = 0; hlnlat.lng.seconds = 0; hlnlat.lng.neg = 0;
-	hlnlat.lat.degrees = 50; hlnlat.lat.minutes = 0; hlnlat.lat.seconds = 0; hlnlat.lat.neg = 0;
+	hlnlat.lng.degrees = 100;
+	hlnlat.lng.minutes = 0;
+	hlnlat.lng.seconds = 0;
+	hlnlat.lng.neg = 0;
+	hlnlat.lat.degrees = 50;
+	hlnlat.lat.minutes = 0;
+	hlnlat.lat.seconds = 0;
+	hlnlat.lat.neg = 0;
 	ln2_hlnlat_to_lnlat(&hlnlat, &lnlat);
-	failed += test_result("(Utility) hlnlat_to_lnlat lng", LN_R2D(lnlat.lng), 100.0, 1e-6);
-	failed += test_result("(Utility) hlnlat_to_lnlat lat", LN_R2D(lnlat.lat), 50.0, 1e-6);
+	failed += test_result("(Utility) hlnlat_to_lnlat lng", LN_R2D(lnlat.lng),
+						  100.0, 1e-6);
+	failed += test_result("(Utility) hlnlat_to_lnlat lat", LN_R2D(lnlat.lat),
+						  50.0, 1e-6);
 
 	/* hrz to nswe */
 	/* 0=N, 90=E? No, 0=S? */
@@ -80,23 +98,14 @@ int utility_conversion_test(void)
 	hrz.az = 0.0;
 	/* printf("DEBUG: Calling ln_hrz_to_nswe\n"); */
 	loc_str = ln_hrz_to_nswe(&hrz);
-	if (loc_str == NULL) {
-		printf("TEST (Utility) hrz_to_nswe NULL....[FAILED]\n");
-		failed++;
-	} else {
-		printf("TEST (Utility) hrz_to_nswe %s....[PASSED]\n", loc_str);
-	}
+	failed += test_str_result("(Utility) hrz_to_nswe 0.0",
+							  loc_str ? loc_str : "NULL", "S");
 
 	/* Also test radians? Az=0 -> S. PI -> N. */
 	hrz.az = M_PI;
 	loc_str = ln_hrz_to_nswe(&hrz);
-	if (strcmp(loc_str, "N") != 0 && strcmp(loc_str, "North") != 0) { /* verify expected string */
-		/* Assuming N based on previous test code */
-		printf("TEST (Utility) hrz_to_nswe N (radians)....[FAILED]\n");
-		failed++;
-	} else {
-		printf("TEST (Utility) hrz_to_nswe N (radians)....[PASSED]\n");
-	}
+	failed += test_str_result("(Utility) hrz_to_nswe PI",
+							  loc_str ? loc_str : "NULL", "N");
 
 	/* Interpolation */
 	res = ln2_interpolate3(0.5, 0.0, 1.0, 2.0);
--- lntest/test_vsop87.c
+++ lntest/test_vsop87.c
@@ -6,180 +6,220 @@
 #include <libnova2/libnova2.h>
 #include <stdio.h>
 
-int vsop87_test(void) {
-  struct ln_helio_posn pos;
-  struct lnh_equ_posn hequ;
-  struct ln_equ_posn equ;
-  double JD = 2448976.5;
-  double au;
-  int failed = 0;
+int vsop87_test(void)
+{
+	struct ln_helio_posn pos;
+	struct ln_equ_posn equ;
+	double JD = 2448976.5;
+	double au;
+	int failed = 0;
 
-  ln2_get_solar_equ_coords(JD, &equ);
-  failed += test_result("(Solar Position) RA on JD 2448976.5  ",
-                        LN_R2D(equ.ra), 268.30941158, 0.00000001);
-  failed += test_result("(Solar Position) DEC on JD 2448976.5  ",
-                        LN_R2D(equ.dec), -23.42585297, 0.00000001);
+	ln2_get_solar_equ_coords(JD, &equ);
+	failed += test_result("(Solar Position) RA on JD 2448976.5  ",
+						  LN_R2D(equ.ra), 268.30941158, 0.00000001);
+	failed += test_result("(Solar Position) DEC on JD 2448976.5  ",
+						  LN_R2D(equ.dec), -23.42585297, 0.00000001);
 
-  ln2_get_mercury_helio_coords(JD, &pos);
-  printf("Mercury L %f B %f R %f\n", pos.L, pos.B, pos.R);
-  ln2_get_mercury_equ_coords(JD, &equ);
-  ln2_equ_to_hequ(&equ, &hequ);
-  printf("Mercury RA %d:%d:%f Dec %d:%d:%f\n", hequ.ra.hours, hequ.ra.minutes,
-         hequ.ra.seconds, hequ.dec.degrees, hequ.dec.minutes, hequ.dec.seconds);
-  au = ln2_get_mercury_earth_dist(JD);
-  fprintf(stdout, "mercury -> Earth dist (AU) %f\n", au);
-  au = ln2_get_mercury_solar_dist(JD);
-  fprintf(stdout, "mercury -> Sun dist (AU) %f\n", au);
-  au = ln2_get_mercury_disk(JD);
-  fprintf(stdout, "mercury -> illuminated disk %f\n", au);
-  au = ln2_get_mercury_magnitude(JD);
-  fprintf(stdout, "mercury -> magnitude %f\n", au);
-  au = ln2_get_mercury_phase(JD);
-  fprintf(stdout, "mercury -> phase %f\n", au);
-  au = ln2_get_mercury_sdiam(JD);
-  fprintf(stdout, "mercury -> sdiam %f\n", au);
+	ln2_get_mercury_helio_coords(JD, &pos);
+	failed += test_result("(Mercury) Helio L  ", pos.L, 3.531730, 0.000001);
+	failed += test_result("(Mercury) Helio B  ", pos.B, 0.053780, 0.000001);
+	failed += test_result("(Mercury) Helio R  ", pos.R, 0.419940, 0.000001);
+	ln2_get_mercury_equ_coords(JD, &equ);
+	failed += test_result("(Mercury) Equ RA   ", LN_R2D(equ.ra), 248.600395,
+						  0.000001);
+	failed += test_result("(Mercury) Equ Dec  ", LN_R2D(equ.dec), -20.906439,
+						  0.000001);
+	au = ln2_get_mercury_earth_dist(JD);
+	failed +=
+		test_result("(Mercury) Earth dist (AU)  ", au, 1.215950, 0.000001);
+	au = ln2_get_mercury_solar_dist(JD);
+	failed +=
+		test_result("(Mercury) Solar dist (AU)  ", au, 0.419940, 0.000001);
+	au = ln2_get_mercury_disk(JD);
+	failed +=
+		test_result("(Mercury) Illuminated disk  ", au, 0.836339, 0.000001);
+	au = ln2_get_mercury_magnitude(JD);
+	failed += test_result("(Mercury) Magnitude  ", au, -1.848023, 0.000001);
+	au = ln2_get_mercury_phase(JD);
+	failed += test_result("(Mercury) Phase  ", au, 0.832975, 0.000001);
+	au = ln2_get_mercury_sdiam(JD);
+	failed += test_result("(Mercury) Sdiam  ", au, 0.000013, 0.000001);
 
-  ln2_get_venus_helio_coords(JD, &pos);
-  printf("Venus L %f B %f R %f\n", pos.L, pos.B, pos.R);
-  ln2_get_venus_equ_coords(JD, &equ);
-  ln2_equ_to_hequ(&equ, &hequ);
-  printf("Venus RA %d:%d:%f Dec %d:%d:%f\n", hequ.ra.hours, hequ.ra.minutes,
-         hequ.ra.seconds, hequ.dec.degrees, hequ.dec.minutes, hequ.dec.seconds);
-  au = ln2_get_venus_earth_dist(JD);
-  fprintf(stdout, "venus -> Earth dist (AU) %f\n", au);
-  au = ln2_get_venus_solar_dist(JD);
-  fprintf(stdout, "venus -> Sun dist (AU) %f\n", au);
-  au = ln2_get_venus_disk(JD);
-  fprintf(stdout, "venus -> illuminated disk %f\n", au);
-  au = ln2_get_venus_magnitude(JD);
-  fprintf(stdout, "venus -> magnitude %f\n", au);
-  au = ln2_get_venus_phase(JD);
-  fprintf(stdout, "venus -> phase %f\n", au);
-  au = ln2_get_venus_sdiam(JD);
-  fprintf(stdout, "venus -> sdiam %f\n", au);
+	ln2_get_venus_helio_coords(JD, &pos);
+	failed += test_result("(Venus) Helio L  ", pos.L, 0.457467, 0.000001);
+	failed += test_result("(Venus) Helio B  ", pos.B, -0.045725, 0.000001);
+	failed += test_result("(Venus) Helio R  ", pos.R, 0.724602, 0.000001);
+	ln2_get_venus_equ_coords(JD, &equ);
+	failed +=
+		test_result("(Venus) Equ RA   ", LN_R2D(equ.ra), 316.269950, 0.000001);
+	failed +=
+		test_result("(Venus) Equ Dec  ", LN_R2D(equ.dec), -18.859699, 0.000001);
+	au = ln2_get_venus_earth_dist(JD);
+	failed += test_result("(Venus) Earth dist (AU)  ", au, 0.910846, 0.000001);
+	au = ln2_get_venus_solar_dist(JD);
+	failed += test_result("(Venus) Solar dist (AU)  ", au, 0.724602, 0.000001);
+	au = ln2_get_venus_disk(JD);
+	failed += test_result("(Venus) Illuminated disk  ", au, 0.646507, 0.000001);
+	au = ln2_get_venus_magnitude(JD);
+	failed += test_result("(Venus) Magnitude  ", au, -5.300746, 0.000001);
+	au = ln2_get_venus_phase(JD);
+	failed += test_result("(Venus) Phase  ", au, 1.273419, 0.000001);
+	au = ln2_get_venus_sdiam(JD);
+	failed += test_result("(Venus) Sdiam  ", au, 0.000045, 0.000001);
 
-  ln2_get_earth_helio_coords(JD, &pos);
-  printf("Earth L %f B %f R %f\n", pos.L, pos.B, pos.R);
-  au = ln2_get_earth_solar_dist(JD);
-  fprintf(stdout, "earth -> Sun dist (AU) %f\n", au);
+	ln2_get_earth_helio_coords(JD, &pos);
+	failed += test_result("(Earth) Helio L  ", pos.L, 1.543810, 0.000001);
+	failed += test_result("(Earth) Helio B  ", pos.B, 0.000008, 0.000001);
+	failed += test_result("(Earth) Helio R  ", pos.R, 0.983824, 0.000001);
+	au = ln2_get_earth_solar_dist(JD);
+	failed += test_result("(Earth) Solar dist (AU)  ", au, 0.983824, 0.000001);
 
-  ln2_get_mars_helio_coords(JD, &pos);
-  printf("Mars L %f B %f R %f\n", pos.L, pos.B, pos.R);
-  ln2_get_mars_equ_coords(JD, &equ);
-  ln2_equ_to_hequ(&equ, &hequ);
-  printf("Mars RA %d:%d:%f Dec %d:%d:%f\n", hequ.ra.hours, hequ.ra.minutes,
-         hequ.ra.seconds, hequ.dec.degrees, hequ.dec.minutes, hequ.dec.seconds);
-  au = ln2_get_mars_earth_dist(JD);
-  fprintf(stdout, "mars -> Earth dist (AU) %f\n", au);
-  au = ln2_get_mars_solar_dist(JD);
-  fprintf(stdout, "mars -> Sun dist (AU) %f\n", au);
-  au = ln2_get_mars_disk(JD);
-  fprintf(stdout, "mars -> illuminated disk %f\n", au);
-  au = ln2_get_mars_magnitude(JD);
-  fprintf(stdout, "mars -> magnitude %f\n", au);
-  au = ln2_get_mars_phase(JD);
-  fprintf(stdout, "mars -> phase %f\n", au);
-  au = ln2_get_mars_sdiam(JD);
-  fprintf(stdout, "mars -> sdiam %f\n", au);
+	ln2_get_mars_helio_coords(JD, &pos);
+	failed += test_result("(Mars) Helio L  ", pos.L, 1.724432, 0.000001);
+	failed += test_result("(Mars) Helio B  ", pos.B, 0.024449, 0.000001);
+	failed += test_result("(Mars) Helio R  ", pos.R, 1.590873, 0.000001);
+	ln2_get_mars_equ_coords(JD, &equ);
+	failed +=
+		test_result("(Mars) Equ RA   ", LN_R2D(equ.ra), 117.241177, 0.000001);
+	failed +=
+		test_result("(Mars) Equ Dec  ", LN_R2D(equ.dec), 24.576727, 0.000001);
+	au = ln2_get_mars_earth_dist(JD);
+	failed += test_result("(Mars) Earth dist (AU)  ", au, 0.648345, 0.000001);
+	au = ln2_get_mars_solar_dist(JD);
+	failed += test_result("(Mars) Solar dist (AU)  ", au, 1.590873, 0.000001);
+	au = ln2_get_mars_disk(JD);
+	failed += test_result("(Mars) Illuminated disk  ", au, 0.980719, 0.000001);
+	au = ln2_get_mars_magnitude(JD);
+	failed += test_result("(Mars) Magnitude  ", au, -1.448334, 0.000001);
+	au = ln2_get_mars_phase(JD);
+	failed += test_result("(Mars) Phase  ", au, 0.278616, 0.000001);
+	au = ln2_get_mars_sdiam(JD);
+	failed += test_result("(Mars) Sdiam  ", au, 0.000035, 0.000001);
 
-  ln2_get_jupiter_helio_coords(JD, &pos);
-  printf("Jupiter L %f B %f R %f\n", pos.L, pos.B, pos.R);
-  ln2_get_jupiter_equ_coords(JD, &equ);
-  ln2_equ_to_hequ(&equ, &hequ);
-  printf("Jupiter RA %d:%d:%f Dec %d:%d:%f\n", hequ.ra.hours, hequ.ra.minutes,
-         hequ.ra.seconds, hequ.dec.degrees, hequ.dec.minutes, hequ.dec.seconds);
-  au = ln2_get_jupiter_earth_dist(JD);
-  fprintf(stdout, "jupiter -> Earth dist (AU) %f\n", au);
-  au = ln2_get_jupiter_solar_dist(JD);
-  fprintf(stdout, "jupiter -> Sun dist (AU) %f\n", au);
-  au = ln2_get_jupiter_disk(JD);
-  fprintf(stdout, "jupiter -> illuminated disk %f\n", au);
-  au = ln2_get_jupiter_magnitude(JD);
-  fprintf(stdout, "jupiter -> magnitude %f\n", au);
-  au = ln2_get_jupiter_phase(JD);
-  fprintf(stdout, "jupiter -> phase %f\n", au);
-  au = ln2_get_jupiter_pol_sdiam(JD);
-  fprintf(stdout, "jupiter -> polar sdiam %f\n", au);
-  au = ln2_get_jupiter_equ_sdiam(JD);
-  fprintf(stdout, "jupiter -> equ sdiam %f\n", au);
+	ln2_get_jupiter_helio_coords(JD, &pos);
+	failed += test_result("(Jupiter) Helio L  ", pos.L, 3.181423, 0.000001);
+	failed += test_result("(Jupiter) Helio B  ", pos.B, 0.022528, 0.000001);
+	failed += test_result("(Jupiter) Helio R  ", pos.R, 5.446765, 0.000001);
+	ln2_get_jupiter_equ_coords(JD, &equ);
+	failed += test_result("(Jupiter) Equ RA   ", LN_R2D(equ.ra), 191.876253,
+						  0.000001);
+	failed += test_result("(Jupiter) Equ Dec  ", LN_R2D(equ.dec), -3.735619,
+						  0.000001);
+	au = ln2_get_jupiter_earth_dist(JD);
+	failed +=
+		test_result("(Jupiter) Earth dist (AU)  ", au, 5.599155, 0.000001);
+	au = ln2_get_jupiter_solar_dist(JD);
+	failed +=
+		test_result("(Jupiter) Solar dist (AU)  ", au, 5.446765, 0.000001);
+	au = ln2_get_jupiter_disk(JD);
+	failed +=
+		test_result("(Jupiter) Illuminated disk  ", au, 0.992256, 0.000001);
+	au = ln2_get_jupiter_magnitude(JD);
+	failed += test_result("(Jupiter) Magnitude  ", au, -1.977813, 0.000001);
+	au = ln2_get_jupiter_phase(JD);
+	failed += test_result("(Jupiter) Phase  ", au, 0.176228, 0.000001);
+	au = ln2_get_jupiter_pol_sdiam(JD);
+	failed += test_result("(Jupiter) Polar Sdiam  ", au, 0.000080, 0.000001);
+	au = ln2_get_jupiter_equ_sdiam(JD);
+	failed += test_result("(Jupiter) Equ Sdiam  ", au, 0.000085, 0.000001);
 
-  ln2_get_saturn_helio_coords(JD, &pos);
-  printf("Saturn L %f B %f R %f\n", pos.L, pos.B, pos.R);
-  ln2_get_saturn_equ_coords(JD, &equ);
-  ln2_equ_to_hequ(&equ, &hequ);
-  printf("Saturn RA %d:%d:%f Dec %d:%d:%f\n", hequ.ra.hours, hequ.ra.minutes,
-         hequ.ra.seconds, hequ.dec.degrees, hequ.dec.minutes, hequ.dec.seconds);
-  au = ln2_get_saturn_earth_dist(JD);
-  fprintf(stdout, "saturn -> Earth dist (AU) %f\n", au);
-  au = ln2_get_saturn_solar_dist(JD);
-  fprintf(stdout, "saturn -> Sun dist (AU) %f\n", au);
-  au = ln2_get_saturn_disk(JD);
-  fprintf(stdout, "saturn -> illuminated disk %f\n", au);
-  au = ln2_get_saturn_magnitude(JD);
-  fprintf(stdout, "saturn -> magnitude %f\n", au);
-  au = ln2_get_saturn_phase(JD);
-  fprintf(stdout, "saturn -> phase %f\n", au);
-  au = ln2_get_saturn_pol_sdiam(JD);
-  fprintf(stdout, "saturn -> polar sdiam %f\n", au);
-  au = ln2_get_saturn_equ_sdiam(JD);
-  fprintf(stdout, "saturn -> equ sdiam %f\n", au);
+	ln2_get_saturn_helio_coords(JD, &pos);
+	failed += test_result("(Saturn) Helio L  ", pos.L, 5.574847, 0.000001);
+	failed += test_result("(Saturn) Helio B  ", pos.B, -0.018846, 0.000001);
+	failed += test_result("(Saturn) Helio R  ", pos.R, 9.867006, 0.000001);
+	ln2_get_saturn_equ_coords(JD, &equ);
+	failed +=
+		test_result("(Saturn) Equ RA   ", LN_R2D(equ.ra), 318.020347, 0.000001);
+	failed += test_result("(Saturn) Equ Dec  ", LN_R2D(equ.dec), -17.232318,
+						  0.000001);
+	au = ln2_get_saturn_earth_dist(JD);
+	failed +=
+		test_result("(Saturn) Earth dist (AU)  ", au, 10.514363, 0.000001);
+	au = ln2_get_saturn_solar_dist(JD);
+	failed += test_result("(Saturn) Solar dist (AU)  ", au, 9.867006, 0.000001);
+	au = ln2_get_saturn_disk(JD);
+	failed +=
+		test_result("(Saturn) Illuminated disk  ", au, 0.998677, 0.000001);
+	au = ln2_get_saturn_magnitude(JD);
+	failed += test_result("(Saturn) Magnitude  ", au, 1.199842, 0.000001);
+	au = ln2_get_saturn_phase(JD);
+	failed += test_result("(Saturn) Phase  ", au, 0.072750, 0.000001);
+	au = ln2_get_saturn_pol_sdiam(JD);
+	failed += test_result("(Saturn) Polar Sdiam  ", au, 0.000034, 0.000001);
+	au = ln2_get_saturn_equ_sdiam(JD);
+	failed += test_result("(Saturn) Equ Sdiam  ", au, 0.000038, 0.000001);
 
-  ln2_get_uranus_helio_coords(JD, &pos);
-  printf("Uranus L %f B %f R %f\n", pos.L, pos.B, pos.R);
-  ln2_get_uranus_equ_coords(JD, &equ);
-  ln2_equ_to_hequ(&equ, &hequ);
-  printf("Uranus RA %d:%d:%f Dec %d:%d:%f\n", hequ.ra.hours, hequ.ra.minutes,
-         hequ.ra.seconds, hequ.dec.degrees, hequ.dec.minutes, hequ.dec.seconds);
-  au = ln2_get_uranus_earth_dist(JD);
-  fprintf(stdout, "uranus -> Earth dist (AU) %f\n", au);
-  au = ln2_get_uranus_solar_dist(JD);
-  fprintf(stdout, "uranus -> Sun dist (AU) %f\n", au);
-  au = ln2_get_uranus_disk(JD);
-  fprintf(stdout, "uranus -> illuminated disk %f\n", au);
-  au = ln2_get_uranus_magnitude(JD);
-  fprintf(stdout, "uranus -> magnitude %f\n", au);
-  au = ln2_get_uranus_phase(JD);
-  fprintf(stdout, "uranus -> phase %f\n", au);
-  au = ln2_get_uranus_sdiam(JD);
-  fprintf(stdout, "uranus -> sdiam %f\n", au);
+	ln2_get_uranus_helio_coords(JD, &pos);
+	failed += test_result("(Uranus) Helio L  ", pos.L, 5.026336, 0.000001);
+	failed += test_result("(Uranus) Helio B  ", pos.B, -0.007518, 0.000001);
+	failed += test_result("(Uranus) Helio R  ", pos.R, 19.570238, 0.000001);
+	ln2_get_uranus_equ_coords(JD, &equ);
+	failed +=
+		test_result("(Uranus) Equ RA   ", LN_R2D(equ.ra), 288.557772, 0.000001);
+	failed += test_result("(Uranus) Equ Dec  ", LN_R2D(equ.dec), -22.757668,
+						  0.000001);
+	au = ln2_get_uranus_earth_dist(JD);
+	failed +=
+		test_result("(Uranus) Earth dist (AU)  ", au, 20.500050, 0.000001);
+	au = ln2_get_uranus_solar_dist(JD);
+	failed +=
+		test_result("(Uranus) Solar dist (AU)  ", au, 19.570238, 0.000001);
+	au = ln2_get_uranus_disk(JD);
+	failed +=
+		test_result("(Uranus) Illuminated disk  ", au, 0.999936, 0.000001);
+	au = ln2_get_uranus_magnitude(JD);
+	failed += test_result("(Uranus) Magnitude  ", au, 5.826755, 0.000001);
+	au = ln2_get_uranus_phase(JD);
+	failed += test_result("(Uranus) Phase  ", au, 0.016051, 0.000001);
+	au = ln2_get_uranus_sdiam(JD);
+	failed += test_result("(Uranus) Sdiam  ", au, 0.000008, 0.000001);
 
-  ln2_get_neptune_helio_coords(JD, &pos);
-  printf("Neptune L %f B %f R %f\n", pos.L, pos.B, pos.R);
-  ln2_get_neptune_equ_coords(JD, &equ);
-  ln2_equ_to_hequ(&equ, &hequ);
-  printf("Neptune RA %d:%d:%f Dec %d:%d:%f\n", hequ.ra.hours, hequ.ra.minutes,
-         hequ.ra.seconds, hequ.dec.degrees, hequ.dec.minutes, hequ.dec.seconds);
-  au = ln2_get_neptune_earth_dist(JD);
-  fprintf(stdout, "neptune -> Earth dist (AU) %f\n", au);
-  au = ln2_get_neptune_solar_dist(JD);
-  fprintf(stdout, "neptune -> Sun dist (AU) %f\n", au);
-  au = ln2_get_neptune_disk(JD);
-  fprintf(stdout, "neptune -> illuminated disk %f\n", au);
-  au = ln2_get_neptune_magnitude(JD);
-  fprintf(stdout, "neptune -> magnitude %f\n", au);
-  au = ln2_get_neptune_phase(JD);
-  fprintf(stdout, "neptune -> phase %f\n", au);
-  au = ln2_get_neptune_sdiam(JD);
-  fprintf(stdout, "neptune -> sdiam %f\n", au);
+	ln2_get_neptune_helio_coords(JD, &pos);
+	failed += test_result("(Neptune) Helio L  ", pos.L, 5.037637, 0.000001);
+	failed += test_result("(Neptune) Helio B  ", pos.B, 0.012159, 0.000001);
+	failed += test_result("(Neptune) Helio R  ", pos.R, 30.188053, 0.000001);
+	ln2_get_neptune_equ_coords(JD, &equ);
+	failed += test_result("(Neptune) Equ RA   ", LN_R2D(equ.ra), 289.414566,
+						  0.000001);
+	failed += test_result("(Neptune) Equ Dec  ", LN_R2D(equ.dec), -21.557733,
+						  0.000001);
+	au = ln2_get_neptune_earth_dist(JD);
+	failed +=
+		test_result("(Neptune) Earth dist (AU)  ", au, 31.113260, 0.000001);
+	au = ln2_get_neptune_solar_dist(JD);
+	failed +=
+		test_result("(Neptune) Solar dist (AU)  ", au, 30.188053, 0.000001);
+	au = ln2_get_neptune_disk(JD);
+	failed +=
+		test_result("(Neptune) Illuminated disk  ", au, 0.999970, 0.000001);
+	au = ln2_get_neptune_magnitude(JD);
+	failed += test_result("(Neptune) Magnitude  ", au, 7.993903, 0.000001);
+	au = ln2_get_neptune_phase(JD);
+	failed += test_result("(Neptune) Phase  ", au, 0.010915, 0.000001);
+	au = ln2_get_neptune_sdiam(JD);
+	failed += test_result("(Neptune) Sdiam  ", au, 0.000005, 0.000001);
 
-  ln2_get_pluto_helio_coords(JD, &pos);
-  printf("Pluto L %f B %f R %f\n", pos.L, pos.B, pos.R);
-  ln2_get_pluto_equ_coords(JD, &equ);
-  ln2_equ_to_hequ(&equ, &hequ);
-  printf("Pluto RA %d:%d:%f Dec %d:%d:%f\n", hequ.ra.hours, hequ.ra.minutes,
-         hequ.ra.seconds, hequ.dec.degrees, hequ.dec.minutes, hequ.dec.seconds);
-  au = ln2_get_pluto_earth_dist(JD);
-  fprintf(stdout, "pluto -> Earth dist (AU) %f\n", au);
-  au = ln2_get_pluto_solar_dist(JD);
-  fprintf(stdout, "pluto -> Sun dist (AU) %f\n", au);
-  au = ln2_get_pluto_disk(JD);
-  fprintf(stdout, "pluto -> illuminated disk %f\n", au);
-  au = ln2_get_pluto_magnitude(JD);
-  fprintf(stdout, "pluto -> magnitude %f\n", au);
-  au = ln2_get_pluto_phase(JD);
-  fprintf(stdout, "pluto -> phase %f\n", au);
-  au = ln2_get_pluto_sdiam(JD);
-  fprintf(stdout, "pluto -> sdiam %f\n", au);
+	ln2_get_pluto_helio_coords(JD, &pos);
+	failed += test_result("(Pluto) Helio L  ", pos.L, 4.070333, 0.000001);
+	failed += test_result("(Pluto) Helio B  ", pos.B, 0.253319, 0.000001);
+	failed += test_result("(Pluto) Helio R  ", pos.R, 29.717796, 0.000001);
+	ln2_get_pluto_equ_coords(JD, &equ);
+	failed +=
+		test_result("(Pluto) Equ RA   ", LN_R2D(equ.ra), 235.390012, 0.000001);
+	failed +=
+		test_result("(Pluto) Equ Dec  ", LN_R2D(equ.dec), -5.121212, 0.000001);
+	au = ln2_get_pluto_earth_dist(JD);
+	failed += test_result("(Pluto) Earth dist (AU)  ", au, 30.501621, 0.000001);
+	au = ln2_get_pluto_solar_dist(JD);
+	failed += test_result("(Pluto) Solar dist (AU)  ", au, 29.717796, 0.000001);
+	au = ln2_get_pluto_disk(JD);
+	failed += test_result("(Pluto) Illuminated disk  ", au, 0.999902, 0.000001);
+	au = ln2_get_pluto_magnitude(JD);
+	failed += test_result("(Pluto) Magnitude  ", au, 13.786698, 0.000001);
+	au = ln2_get_pluto_phase(JD);
+	failed += test_result("(Pluto) Phase  ", au, 0.019749, 0.000001);
+	au = ln2_get_pluto_sdiam(JD);
+	failed += test_result("(Pluto) Sdiam  ", au, 0.000000, 0.000001);
 
-  return failed;
+	return failed;
 }
